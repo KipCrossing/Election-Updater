2891d1ce44a739e6a1d91d8374b1be47293333f1
Max Kaye <m@xk.io>
2019-03-27 10:25:03 +1100

    start refactor

--- ecupdate.py
+++ ecupdate.py
@@ -3,15 +3,14 @@ import discord
 from discord.ext import commands
 import asyncio
 from itertools import cycle
+import os
 
 from selenium import webdriver
 import time
 import pandas as pd
 
 
-
-
-TOKEN = 'NTYwMDY3OTMyNDExOTIwMzg2.D3ujrQ.ZZc5cyGfwrNjvHKZ4tgSdSR1ZNg'
+TOKEN = os.environ.get('DISCORD_BOT_TOKEN', None) # 'NTYwMDY3OTMyNDExOTIwMzg2.D3ujrQ.ZZc5cyGfwrNjvHKZ4tgSdSR1ZNg'
 
 client = commands.Bot(command_prefix = '!')
 
@@ -22,8 +21,16 @@ async def update_score():
         try:
             driver = webdriver.Chrome()
             driver.get('https://vtr.elections.nsw.gov.au/lc/state/cc/fp_summary')
+            
+            # let it load; todo: does selenium have an await page load function?
             time.sleep(2)
-            df = pd.read_html(driver.page_source)[8]
+
+            vtr_html = driver.page_source
+
+            # extracts tables from html; group I is 9th -> index = 8
+            df = pd.read_html(vtr_html)[8]
+            
+
             new_score = df[list(df)[2]][16]
             percent_votes = df[list(df)[3]][16]
             driver.close()
