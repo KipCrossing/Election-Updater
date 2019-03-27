5d29bc0cdfad84e6908a365d22a0a750c8ecf790
Max Kaye <m@xk.io>
2019-03-27 14:18:38 +1100

    maxs patches and things

--- ecupdate.py
+++ ecupdate.py
@@ -1,48 +1,96 @@
 print("Flux Discord Bot")
-import discord
-from discord.ext import commands
 import asyncio
 from itertools import cycle
 import os
-
-from selenium import webdriver
 import time
+import sys
+
+import discord
+from discord.ext import commands
+from selenium import webdriver
 import pandas as pd
+from bs4 import BeautifulSoup
 
 
-TOKEN = os.environ.get('DISCORD_BOT_TOKEN', None) # 'NTYwMDY3OTMyNDExOTIwMzg2.D3ujrQ.ZZc5cyGfwrNjvHKZ4tgSdSR1ZNg'
-
+TOKEN = os.environ.get('DISCORD_BOT_TOKEN', None)
 client = commands.Bot(command_prefix = '!')
+print('loaded client')
+
+
+async def update_votes_inner():
+    chrome_opts = webdriver.ChromeOptions()
+    chrome_opts.add_argument('--headless')
+    chrome_opts.add_argument('--no-sandbox')
+    chrome_opts.add_argument('--disable-dev-shm-usage')
+    try:
+        driver = webdriver.Chrome(chrome_options=chrome_opts)
+    except Exception as e:
+        driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', chrome_options=chrome_opts)
+    driver.get('https://vtr.elections.nsw.gov.au/lc/state/cc/fp_summary')
+    
+    # let it load; todo: does selenium have an await page load function?
+    # let it be a long wait because better than failing
+    time.sleep(2)
+
+    # html of page; after it's been modified by JS
+    vtr_html = driver.page_source
+    vtr_soup = BeautifulSoup(vtr_html, 'html.parser')
+    
+    # don't need this anymore
+    driver.close()
+
+    report_content = vtr_soup.find(id="ReportContent")
+    # p tags in report_content
+    report_ps = report_content.find_all("p")
+    last_updated = report_ps[0].string
+
+    # check last updated; create file using a+ first if need be
+    with open('__last_updated_tag__.txt', 'a+') as f:
+        pass
+    with open('__last_updated_tag__.txt', 'r') as f:
+        cached_updated = str(f.read())
+        if cached_updated == last_updated:
+            print('no updates; exiting')
+            return
+        else:
+            print("diff:", cached_updated, last_updated, sep='|\n')
+    with open('__last_updated_tag__.txt', 'w+') as f:
+        f.write(last_updated)
+    
+    # grab the number of votes per quota
+    quota_votes = int(list(report_ps[2].strings)[1].replace(',',''))
+    # a quota is 4.55% or so; we can use this to figure out pct votes counted
+    final_quota_votes = 5 * 10**6 * 0.0455
+    # the * 10000 // 100 thing here will round to 2 dps
+    pct_votes_counted = f'{(quota_votes / final_quota_votes * 10000) // 100} %'
+
+    print(last_updated, quota_votes, sep=" | ")
+
+    # extracts tables from html; group I is 9th -> index = 8
+    flux_group_table_headings = pd.read_html(vtr_html)[8]
+    hdrs = list(flux_group_table_headings)[2:5]
+    hdr_votes, hdr_votes_pct, hdr_votes_quota = hdrs
+
+    # get last row; total votes and quotas
+    new_votes, percent_votes, quotas = [flux_group_table_headings[hdr][16] for hdr in hdrs]
+    print(new_votes, percent_votes, quotas, sep=" | ")
+
+    discord_msg = f"**UPDATE:** *Group Total* \nVotes: **{new_votes}** \n% of Votes: **{percent_votes}**\nQuotas: **{quotas}**\n% *all* votes counted **{pct_votes_counted}**"
+    print(discord_msg)
+
+    await client.send_message(discord.Object('560067038349885441'), discord_msg)
+
 
 async def update_score():
     await client.wait_until_ready()
-    score = ''
-    while not client.is_closed:
-        try:
-            driver = webdriver.Chrome()
-            driver.get('https://vtr.elections.nsw.gov.au/lc/state/cc/fp_summary')
-            
-            # let it load; todo: does selenium have an await page load function?
-            time.sleep(2)
-
-            vtr_html = driver.page_source
-
-            # extracts tables from html; group I is 9th -> index = 8
-            df = pd.read_html(vtr_html)[8]
-            
-
-            new_score = df[list(df)[2]][16]
-            percent_votes = df[list(df)[3]][16]
-            driver.close()
-        except Exception as e:
-            print(e)
-        if new_score != score:
-            score = new_score
-            print(score)
-            await client.send_message(discord.Object('560067038349885441'), \
-            "**UPDATE:** *Group Total* \nVotes: **{}** \n% of Votes: **{}**".format(str(score),str(percent_votes)))
-        await asyncio.sleep(60)
-
+    try:
+        await update_votes_inner()
+    except Exception as e:
+        print('Got exception')
+        print(e)
+        import traceback
+        traceback.print_tb(e.__traceback__)
+    await client.close()
 
 client.loop.create_task(update_score())
 client.run(TOKEN)
